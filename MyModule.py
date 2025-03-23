704b4c2073cbc0400414d563db1173abc8fdbff1
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2025-03-23 19:17:44 +0200

    unit

--- MyModule.py
+++ MyModule.py
@@ -1,16 +1,79 @@
-def vastus(login:str)->bool:
-    """Kasutaja on registreeritud või registreerimata
-    Kasutaja siseneb kontole, 
-    peate sisselogimiseks sisestama oma kasutajatunnuse ja parooli.
-    Kasutaja siseneb , soovid luua peab sisestama sisselogimise ja parooli.
-    (parooli võib sisestada ise või arvuti genereerib selle)
-    :param str login: Sisend kasutajalt on konto või soovid luua
+login_list=["gh"]
+parool_list=["9"]
+import random
+def vas(vastus:str)->bool:
+    """Kas olete registreeritud?
+    Kasutaja siseneb kontole
+    :param str vastus: Sisend kasutajalt on konto või soovid luua
     :rtype: bool tagastab tõeväärsuses formaadis tulemus
     """
-    if login=="on konto": 
+    if vastus=="jah": 
         v=True
     else: 
         v=False
-    return vastus(v)
+    return v
+
+def log(login:str)->bool:
+    """Kas süsteemis on sisselogimine?
+    Kasutaja sisestab kasutajanime.
+    :param str login: Sisend kasutajalt oma kasuutajanimi.
+    :rtype: bool tagastab tõeväärsuses formaadis tulemus
+    """
+    if login in login_list:
+        l=True
+    else:
+        l=False
+    return l
+
+def par(parool: str)->bool:
+    """Kas süsteemis on parool?
+    Kasutaja sisestab parooli
+    :param str login: Kasutaja sisestab oma parooli.
+    :rtype: bool tagastab tõeväärsuses formaadis tulemus
+    """
+    if parool in parool_list:
+        p=True
+    else:
+        p=False
+    return p 
+
+def login_parool(login: str, parool:str)->bool:
+    """
+    """
+    if login in login_list:
+        i=login_list.index(login)
+        parool_list[i]==parool
+        lp=True
+    else:
+        lp=False
+    return lp
+
+def vas2(vastus1:str)->bool:
+    """
+    """
+    if vastus1=="jah":
+        v1=True
+    else:
+        v1=False
+    return v1
+
+def par1(parool1:str)-> str:
+    """
+    """
+    str_list = list(".,:;!_*-+()/#¤%&")
+    par_list=list(parool1)
+    password_ = ""
+    for i in parool1:
+        if i.isupper() or i.isidigit() or i in str_list:
+            password_+=i
+        if 8 => len(password_):
+            v0=True
+        else:
+            v0=False
+        return v0
+
+
+
+
 
 
--- Registreerimine_ja_autoriseerimine.py
+++ Registreerimine_ja_autoriseerimine.py
@@ -1,7 +1,59 @@
+from math import fabs
+from MyModule import *
+import random
 print("Tere tulemast!")
 while 1:
-    try:
-        vastus=input("Sul on konto või soovid seda luua: ").lower()
-
-    
+        vastus=str(input("Kas olete registreeritud? ")).lower()
+        if vastus=="jah" or vastus=="ei": break
+        else:
+            print("Ответ должен быть только да или нет!")
+v=vas(vastus)
+if v:
+    while 1:
+        login=str(input("Введите свой логин: "))
+        if log(login): 
+            break
+        else:
+          print("Seda kasutajanime ei ole nimekirjas!")
+          login_list.append(login)
+    while 1:
+        parool=str(input("Введите свой пароль: "))
+        if par(parool) and login_parool(login, parool): break
+        else:
+            print("Пароль не подходит!")
+        parool_list.append(parool)
+    print("Вы вошли в систему!")
+else:
+    while 1:
+        login=str(input("Введите свой логин: "))
+        if log(login): 
+             print("See kasutajanimi on juba hõivatud, sisesta teine!")
+        else: break
+    login_list.append(login)
+    while 1:
+        vastus1=str(input("Сгенирировать пароль? ")).lower()
+        if vastus=="jah" or vastus=="ei": break
+        else:
+            print("Ответ должен быть только да или нет!")
+    v=vas2(vastus1)
+    if v:
+        str0=".,:;!_*-+()/#¤%&"
+        str1 = '0123456789'
+        str2 = 'qwertyuiopasdfghjklzxcvbnm'
+        str3 = str2.upper()
+        str4 = str0+str1+str2+str3
+        ls = list(str4)
+        password = ''.join([random.choice(ls) for x in range(8)])
+        parool_list.append(password)
+        print(f"Ваш пароль {password}")
+        print("Вы зарегистрировались!")
+    else:
+        while 1:
+            parool1=str(input("Введите свой пароль: "))
+            if len(parool1)<8:
+                vh=par1(parool1)
+                parool_list.append(vh)
+            else:
+                print("Пароль должен состоять как минимум из 8 символов! ")
+        print("Вы зарегистрированы")
        
